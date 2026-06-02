4405055986603427b4ac72bcb04f9da60a3a8c96
kenoztur <kenoztur@gmail.com>
2026-06-03 02:38:25 +0300

    The codes have been successfully and securely uploaded

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 99)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar(i / 10 + '0');
+			ft_putchar(i % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(j / 10 + '0');
+			ft_putchar(j % 10 + '0');
+			if (!(i == 98 && j == 99))
+				write(1, ", ", 2);
+			j++;
+		}
+		i++;
+	}
+}
+
+/*
+int main(void)
+{
+	ft_print_comb2();
+}
+*/
